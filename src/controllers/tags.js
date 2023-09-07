c89650af737c7eee6046c2cf0541db234190e168
Aditya Ganesh <adityawolverines@gmail.com>
2023-09-07 10:15:32 -0400

    fixed some typings to remove the lint suppressions of explicit-any

--- src/controllers/tags.js
+++ src/controllers/tags.js
@@ -1,4 +1,5 @@
 "use strict";
+// req import, logging output with console
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
@@ -31,9 +32,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const validator = __importStar(require("validator"));
-const nconf = __importStar(require("nconf"));
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const validator_1 = __importDefault(require("validator"));
+const nconf_1 = __importDefault(require("nconf"));
 const meta = __importStar(require("../meta"));
 const user = __importStar(require("../user"));
 const categories = __importStar(require("../categories"));
@@ -42,66 +45,88 @@ const privileges = __importStar(require("../privileges"));
 const pagination = __importStar(require("../pagination"));
 const utils = __importStar(require("../utils"));
 const helpers = __importStar(require("./helpers"));
-const tagsController = module.exports;
-tagsController.getTag = function (req, res) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const tag = validator.escape(utils.cleanUpTag(req.params.tag, meta.config.maximumTagLength));
-        const page = parseInt(req.query.page, 10) || 1;
-        const cid = Array.isArray(req.query.cid) || !req.query.cid ? req.query.cid : [req.query.cid];
-        const templateData = {
-            topics: [],
-            tag: tag,
-            breadcrumbs: helpers.buildBreadcrumbs([{ text: '[[tags:tags]]', url: '/tags' }, { text: tag }]),
-            title: `[[pages:tag, ${tag}]]`,
-            allCategoriesUrl: `tags/${tag}${helpers.buildQueryString(req.query, 'cid', '')}`,
-            rssFeedUrl: `${nconf.get('relative_path')}/tags/${tag}.rss`,
-        };
-        const [settings, cids, categoryData, isPrivileged] = yield Promise.all([
-            user.getSettings(req.uid),
-            cid || categories.getCidsByPrivilege('categories:cid', req.uid, 'topics:read'),
-            helpers.getSelectedCategory(cid),
-            user.isPrivileged(req.uid),
-        ]);
-        const start = Math.max(0, (page - 1) * settings.topicsPerPage);
-        const stop = start + settings.topicsPerPage - 1;
-        const [topicCount, tids] = yield Promise.all([
-            topics.getTagTopicCount(tag, cids),
-            topics.getTagTidsByCids(tag, cids, start, stop),
-        ]);
-        templateData.topics = yield topics.getTopics(tids, req.uid);
-        templateData.showSelect = isPrivileged;
-        templateData.showTopicTools = isPrivileged;
-        topics.calculateTopicIndices(templateData.topics, start);
-        res.locals.metaTags = [
-            {
-                name: 'title',
-                content: tag,
-            },
-            {
-                property: 'og:title',
-                content: tag,
-            },
-        ];
-        const pageCount = Math.max(1, Math.ceil(topicCount / settings.topicsPerPage));
-        templateData.pagination = pagination.create(page, pageCount, req.query);
-        helpers.addLinkTags({ url: `tags/${tag}`, res: req.res, tags: templateData.pagination.rel });
-        templateData.feeds = { disableRSS: meta.config['feeds:disableRSS'] };
-        res.render('tag', templateData);
-    });
-};
-tagsController.getTags = function (req, res) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const cids = yield categories.getCidsByPrivilege('categories:cid', req.uid, 'topics:read');
-        const [canSearch, tags] = yield Promise.all([
-            privileges.global.can('search:tags', req.uid),
-            topics.getCategoryTagsData(cids, 0, 99),
-        ]);
-        res.render('tags', {
-            tags: tags.filter(Boolean),
-            displayTagSearch: canSearch,
-            nextStart: 100,
-            breadcrumbs: helpers.buildBreadcrumbs([{ text: '[[tags:tags]]' }]),
-            title: '[[pages:tags]]',
+const tagsController = {
+    getTag: function (req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const tag = validator_1.default.escape(
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            String(utils.cleanUpTag(req.params.tag, meta.config.maximumTagLength)));
+            const page = parseInt(req.query.page, 10) || 1;
+            const cid = (Array.isArray(req.query.cid) ? req.query.cid : [req.query.cid]);
+            const templateData = {
+                topics: [],
+                tag: tag,
+                breadcrumbs: [{
+                        text: '[[tags:tags]]',
+                        url: '/tags',
+                    }, {
+                        text: tag,
+                    }],
+                title: `[[pages:tag, ${tag}]]`,
+            };
+            const [settings, cids, categoryData, isPrivileged] = yield Promise.all([
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+                user.getSettings(req.uid),
+                cid || categories.getCidsByPrivilege('categories:cid', req.uid, 'topics:read'),
+                helpers.getSelectedCategory(cid),
+                user.isPrivileged(req.uid),
+            ]);
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            const start = Math.max(0, (page - 1) * settings.topicsPerPage);
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            const stop = start + (settings.topicsPerPage) - 1;
+            const [topicCount, tids] = yield Promise.all([
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+                topics.getTagTopicCount(tag, cids),
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+                topics.getTagTidsByCids(tag, cids, start, stop),
+            ]);
+            templateData.topics = (yield topics.getTopics(tids, req.uid));
+            templateData.showSelect = isPrivileged;
+            templateData.showTopicTools = isPrivileged;
+            templateData.allCategoriesUrl = `tags/${tag}${helpers.buildQueryString(req.query, 'cid', '')}`;
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            templateData.selectedCategory = categoryData.selectedCategory;
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+            templateData.selectedCids = categoryData.selectedCids;
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+            topics.calculateTopicIndices(templateData.topics, start);
+            res.locals.metaTags = [
+                {
+                    name: 'title',
+                    content: tag,
+                },
+                {
+                    property: 'og:title',
+                    content: tag,
+                },
+            ];
+            const pageCount = Math.max(1, Math.ceil(topicCount / settings.topicsPerPage));
+            templateData.pagination = pagination.create(page, pageCount, req.query);
+            helpers.addLinkTags({ url: `tags/${tag}`, res: req.res, tags: templateData.pagination.rel });
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            templateData['feeds:disableRSS'] = meta.config['feeds:disableRSS'];
+            const relativePath = nconf_1.default.get('relative_path');
+            templateData.rssFeedUrl = `${relativePath}/tags/${tag}.rss`;
+            res.render('tag', templateData);
         });
-    });
+    },
+    getTags: function (req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const cids = yield categories.getCidsByPrivilege('categories:cid', req.uid, 'topics:read');
+            const [canSearch, tags] = yield Promise.all([
+                privileges.global.can('search:tags', req.uid),
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+                topics.getCategoryTagsData(cids, 0, 99),
+            ]);
+            res.render('tags', {
+                tags: tags.filter(Boolean),
+                displayTagSearch: canSearch,
+                nextStart: 100,
+                breadcrumbs: helpers.buildBreadcrumbs([{ text: '[[tags:tags]]' }]),
+                title: '[[pages:tags]]',
+            });
+        });
+    },
 };
+module.exports = tagsController;
--- src/controllers/tags.ts
+++ src/controllers/tags.ts
@@ -1,6 +1,8 @@
-import * as validator from 'validator';
-import * as nconf from 'nconf';
+// req import, logging output with console
 
+import { Request, Response } from 'express';
+import validator from 'validator';
+import nconf from 'nconf';
 import * as meta from '../meta';
 import * as user from '../user';
 import * as categories from '../categories';
@@ -10,86 +12,154 @@ import * as pagination from '../pagination';
 import * as utils from '../utils';
 import * as helpers from './helpers';
 
+interface Crumb {
+    text: string;
+    url: string;
+}
+
+interface Settings {
+    topicsPerPage: number;
+}
+
+interface CustomRequest extends Request {
+    uid: number;
+}
+
+interface SelectedCategory {
+    icon: string;
+    name: string;
+    bgColor: string;
+}
+
+interface CategoryData {
+    selectedCategory?: SelectedCategory | null;
+    selectedCids?: number[];
+}
+
+interface Pagination {
+    rel: never[];
+    pages: {
+        page: number;
+        active: boolean;
+        qs: string;
+    }[];
+    currentPage: number;
+    pageCount: number;
+}
 
 interface TemplateData {
-    topics: any[];
+    topics: string[];
     tag: string;
-    breadcrumbs: any[];
+    breadcrumbs: Crumb[];
     title: string;
     showSelect?: boolean;
     showTopicTools?: boolean;
-    allCategoriesUrl: string;
-    selectedCategory?: any;
-    selectedCids?: any[];
-    pagination?: any;
-    rssFeedUrl: string;
-    feeds?: any;
+    allCategoriesUrl?: string;
+    pagination?: Pagination;
+    rssFeedUrl?: string;
+    'feeds:disableRSS'?: boolean;
+    selectedCategory?: SelectedCategory | null;
+    selectedCids?: number[];
 }
 
-const tagsController = module.exports;
+const tagsController: {
+    getTag: (req: CustomRequest, res: Response) => Promise<void>;
+    getTags: (req: CustomRequest, res: Response) => Promise<void>;
+    } = {
+        getTag: async function (req: CustomRequest, res: Response) {
+            const tag: string = validator.escape(
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                String(utils.cleanUpTag(req.params.tag, meta.config.maximumTagLength))
+            );
+            const page: number = parseInt(req.query.page as string, 10) || 1;
+            const cid: string | string[] = (
+                Array.isArray(req.query.cid) ? req.query.cid : [req.query.cid]
+                ) as string | string[];
 
-tagsController.getTag = async function (req: any, res: any) {
-    const tag = validator.escape(utils.cleanUpTag(req.params.tag, meta.config.maximumTagLength));
-    const page = parseInt(req.query.page, 10) || 1;
-    const cid = Array.isArray(req.query.cid) || !req.query.cid ? req.query.cid : [req.query.cid];
+            const templateData: TemplateData = {
+                topics: [],
+                tag: tag,
+                breadcrumbs: [{
+                    text: '[[tags:tags]]',
+                    url: '/tags',
+                }, {
+                    text: tag,
+                }] as {
+                    text: string;
+                    url: string;
+                }[],
+                title: `[[pages:tag, ${tag}]]`,
+            };
 
-    const templateData: TemplateData = {
-        topics: [],
-        tag: tag,
-        breadcrumbs: helpers.buildBreadcrumbs([{ text: '[[tags:tags]]', url: '/tags' }, { text: tag }]),
-        title: `[[pages:tag, ${tag}]]`,
-        allCategoriesUrl: `tags/${tag}${helpers.buildQueryString(req.query, 'cid', '')}`,
-        rssFeedUrl: `${nconf.get('relative_path')}/tags/${tag}.rss`,
+            const [settings, cids, categoryData, isPrivileged]: [Settings, string | string[], CategoryData,
+            boolean] = await Promise.all([
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+                user.getSettings(req.uid) as Settings,
+                cid || categories.getCidsByPrivilege('categories:cid', req.uid, 'topics:read') as string | string[],
+                helpers.getSelectedCategory(cid),
+                user.isPrivileged(req.uid) as boolean,
+            ]);
+
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            const start: number = Math.max(0, (page - 1) * settings.topicsPerPage);
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            const stop: number = start + (settings.topicsPerPage) - 1;
+
+            const [topicCount, tids]: [number, number[]] = await Promise.all([
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+                topics.getTagTopicCount(tag, cids) as number,
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+                topics.getTagTidsByCids(tag, cids, start, stop) as number[],
+            ]);
+
+            templateData.topics = await topics.getTopics(tids, req.uid) as string[];
+            templateData.showSelect = isPrivileged;
+            templateData.showTopicTools = isPrivileged;
+            templateData.allCategoriesUrl = `tags/${tag}${helpers.buildQueryString(req.query, 'cid', '')}`;
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            templateData.selectedCategory = categoryData.selectedCategory;
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+            templateData.selectedCids = categoryData.selectedCids;
+
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+            topics.calculateTopicIndices(templateData.topics, start);
+
+            res.locals.metaTags = [
+                {
+                    name: 'title',
+                    content: tag,
+                },
+                {
+                    property: 'og:title',
+                    content: tag,
+                },
+            ];
+            const pageCount: number = Math.max(1, Math.ceil(topicCount / settings.topicsPerPage));
+            templateData.pagination = pagination.create(page, pageCount, req.query);
+            helpers.addLinkTags({ url: `tags/${tag}`, res: req.res, tags: templateData.pagination.rel });
+
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            templateData['feeds:disableRSS'] = meta.config['feeds:disableRSS'] as boolean;
+            const relativePath: string = nconf.get('relative_path') as string;
+            templateData.rssFeedUrl = `${relativePath}/tags/${tag}.rss`;
+            res.render('tag', templateData);
+        },
+
+        getTags: async function (req: CustomRequest, res: Response) {
+            const cids = await categories.getCidsByPrivilege('categories:cid', req.uid, 'topics:read') as number[];
+            const [canSearch, tags]: [boolean, string[]] = await Promise.all([
+                privileges.global.can('search:tags', req.uid) as boolean,
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+                topics.getCategoryTagsData(cids, 0, 99) as string[],
+            ]);
+
+            res.render('tags', {
+                tags: tags.filter(Boolean),
+                displayTagSearch: canSearch,
+                nextStart: 100,
+                breadcrumbs: helpers.buildBreadcrumbs([{ text: '[[tags:tags]]' }]),
+                title: '[[pages:tags]]',
+            });
+        },
     };
-    const [settings, cids, categoryData, isPrivileged] = await Promise.all([
-        user.getSettings(req.uid),
-        cid || categories.getCidsByPrivilege('categories:cid', req.uid, 'topics:read'),
-        helpers.getSelectedCategory(cid),
-        user.isPrivileged(req.uid),
-    ]);
-    const start = Math.max(0, (page - 1) * settings.topicsPerPage);
-    const stop = start + settings.topicsPerPage - 1;
-
-    const [topicCount, tids] = await Promise.all([
-        topics.getTagTopicCount(tag, cids),
-        topics.getTagTidsByCids(tag, cids, start, stop),
-    ]);
-
-    templateData.topics = await topics.getTopics(tids, req.uid);
-    templateData.showSelect = isPrivileged;
-    templateData.showTopicTools = isPrivileged;
-    topics.calculateTopicIndices(templateData.topics, start);
-    res.locals.metaTags = [
-        {
-            name: 'title',
-            content: tag,
-        },
-        {
-            property: 'og:title',
-            content: tag,
-        },
-    ];
-
-    const pageCount = Math.max(1, Math.ceil(topicCount / settings.topicsPerPage));
-    templateData.pagination = pagination.create(page, pageCount, req.query);
-    helpers.addLinkTags({ url: `tags/${tag}`, res: req.res, tags: templateData.pagination.rel });
-
-    templateData.feeds = { disableRSS: meta.config['feeds:disableRSS'] };
-    res.render('tag', templateData);
-};
-
-tagsController.getTags = async function (req: any, res: any) {
-    const cids = await categories.getCidsByPrivilege('categories:cid', req.uid, 'topics:read');
-    const [canSearch, tags] = await Promise.all([
-        privileges.global.can('search:tags', req.uid),
-        topics.getCategoryTagsData(cids, 0, 99),
-    ]);
-
-    res.render('tags', {
-        tags: tags.filter(Boolean),
-        displayTagSearch: canSearch,
-        nextStart: 100,
-        breadcrumbs: helpers.buildBreadcrumbs([{ text: '[[tags:tags]]' }]),
-        title: '[[pages:tags]]',
-    });
-};
+export = tagsController;
